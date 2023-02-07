dc63da8ff9ea576e5ff622cabebc6ead50ea1711
Umair Azmat <devumair260@gmail.com>
2023-02-07 21:23:48 +0500

    Js class 3+4

---
+++ Javascript/Class 03/Readme.md
@@ -0,0 +1,33 @@
+Program1
+write a js program, define two variables and sum those 
+two variable and save result in third variable and print in console?
+
+Program2
+write a js program, that can divide two numbers and show 
+the result in console?
+console.log("result of firstProgram", a);
+
+Program3
+write a js program, that can add two number, divide result
+by 5 and show in console?
+console.log("result of firstProgram", a);
+
+Program4
+write a js program, that can convert temp from F into C and show into console, 
+using C = (F − 32) * 5/9 
+use if F = 50, what will be the value of C? 
+
+console.log("Divide two number", a);
+
+
+Working with variables
+
+Declare two variables: admin and name.
+Assign the value "Your Name" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output Your Name).
+
+Giving the right name
+
+Create a variable with the name of our planet. How would you name such a variable?
+Create a variable to store the name of a current visitor to a website. How would you name that variable?
---
+++ Javascript/Class 03/script.js
@@ -0,0 +1,101 @@
+// console.log('Hello World!')
+
+// alert('Hello World!');
+// ["Welocme to" , "JS Class"].forEach(alert)
+
+// alert("Welcome TO JS Class");
+
+
+
+// Write a Js Program   Single Line Comment
+
+/*
+Welcome to JS Class  Multi Line Comment
+3rd Class
+*/
+
+/**
+ * 
+ * @param {value 1 number} a  jis ki power brhani hy 
+ * @param {value 2 number} b  power ki value
+ * @returns power of a and b
+ */
+
+// function power(a,b) {
+//     return a ** b;
+// }
+
+// console.log(power(2,3));
+
+
+// var number; //var declaration
+// number = 10;  //var initilization
+// console.log(number);
+
+// var result ;
+// result = 89.5;
+// console.log(result);
+
+// var firstName;
+// firstName = "Hassan";
+// console.log(firstName);
+
+// var lastName = "Khan";
+// var lastName = "Baloch";
+// console.log(lastName);
+
+// let name = "Hassan"; age = 20 ; 
+// console.log(name , age);
+
+// const Pi = 3.14;
+// Pi = 3.15;
+// console.log(Pi);
+
+// var 123Name = "Hassan";
+// var -name = "Hassan";
+// var first name = "Hassan";
+// var first-Name = "Hassan"; 
+// var %name = "Hassan";
+// var function = "Hassan";
+
+// var first_name = "Hassan";
+// var lastName = "Khan";
+// var age = 20;
+// var $name = "Hassan";
+
+// var dob = 1999;
+// var Dob = 2000;
+
+
+
+// let nameofStudent = "Hassan";
+// console.log(typeof nameofStudent);
+
+// let age = 20;
+// console.log(typeof age);
+
+// let ageofStudent = 18;
+// console.log(ageofStudent > 20);
+
+// const software = [ "Ahmed", 4503, true, "Morning" , 3.14];
+// console.log(software);
+
+// confirm("Are you sure you want to delete this file?");
+
+// var inputName = prompt("Enter your name");
+// // console.log(inputName);
+// alert("Welcome " + inputName);
+
+console.log(2+3);
+console.log(2-7);
+console.log(2*3);
+console.log(2/3);
+console.log(2%3);
+console.log(2**3);
+
+var x= BigInt(28319823918230129309123);
+var a = 28319823918230129309123n;
+
+=
+== 
+===
