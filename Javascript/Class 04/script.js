dd7bb98ed50005df437dcc1f827a5a138d16a48b
Umair Azmat <umairazmat8260@gmail.com>
2023-02-13 22:08:55 +0500

    class 04

---
+++ Javascript/Class 04/script.js
@@ -0,0 +1,59 @@
+// Write a JavaScript program to find the sum of two numbers obtained from
+// prompt and display the result using alert.
+
+// var firstNumber = parseInt(prompt("Enter first number"));
+// var secondNumber = parseInt(prompt("Enter second number"));
+// var sum = firstNumber + secondNumber ;
+// alert(sum);
+// console.log(sum);
+
+// Arithmetic Operators 
+// +, -, *, /, %, ++, --
+
+// let a = 20;
+// let b = 30;
+// let c= a + b; 
+// alert(c);
+// let d = a-b;
+// console.log(a-b);
+// console.log(c);
+// let e = a*b;
+// alert(e);
+// let f = a/b;
+
+// let a = 1;
+// console.log(a); //1
+// console.log(++a); // prefix increment
+// console.log(a++); // postfix increment
+// console.log(a); 
+
+// let a =100;
+// console.log(a); //100
+// console.log(--a); // prefix decrement //99
+// console.log("at line no 33",a--); // postfix decrement
+//  console.log(a);
+
+// let a = 100;    // assignment
+// // == , === 
+
+// "100" == 100; // true
+// "100" === 100; // false
+
+// Assignment Operators
+
+// let number = 100;
+// number += 20 ;
+// console.log(number);
+
+// let num1 = 100;
+// let num2 = 200;
+// console.log(num1 < num2);
+
+// let a =2;
+// let b= 20; 
+
+// if (a > b) {
+//     console.log("a is greater than b");
+// } else {
+//     console.log("a is less than b");
+// }
