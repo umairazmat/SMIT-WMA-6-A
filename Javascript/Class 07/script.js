a10e2d76488ccb681210fb85c9be0a6ac63cfab6
Umair Azmat <umairazmat8260@gmail.com>
2023-02-21 23:49:26 +0500

    class 07 JS

---
+++ Javascript/Class 07/script.js
@@ -0,0 +1,132 @@
+// let a = 2+2;
+
+// num === str  not equal 
+
+// let b = prompt('Enter a number','');
+
+// switch (b) {
+//     case 1:
+//         alert('value is too small')
+//         break;
+//         case 2:
+//             alert('value is  small')
+//             break;
+//             case 3:
+//                 alert('value is  near but small')
+//                 break;
+//                 case 4:
+//                     alert('Exact Value')
+//                    break;
+                   
+//                     case 5:
+//                         alert('value is  near but big')
+//                     break;
+//     default:
+//         alert('Invalid Value')
+//         break;
+// }
+
+
+// let num1 = 9;
+// let num2 = 88;
+
+// sitch ===
+
+// switch (true) {
+//     case num1 > num2:
+//         alert('num1 is greater than num2')
+//         break;
+//     case num1 < num2:
+//         alert('num1 is less than num2')
+//         break;
+//     default:
+//         alert('Invalid Value')
+//         break;
+// }
+
+
+// switch (num1 > num2) {
+//     case true:
+//         alert('num1 is greater than num2')
+//         break;
+//     case false:
+//         alert('num1 is less than num2')
+//         break;
+//     default:
+//         alert('Invalid Value')
+//         break;
+// }
+
+
+
+// let day = Date().slice(0,3);
+// console.log(day)
+// switch (day) {
+//     case "Mon":
+//         console.log("Monday");
+//         break;
+//         case "Tue":
+//             console.log("Tuesday");
+//             break;
+//             case "Wed":
+//                 console.log("Wednesday");
+//                 break;
+//                 case "Thu":
+//                     console.log("Thursday");
+//                     break;
+//                     case "Fri":
+//                         console.log("Friday");
+//                         break;
+//                         case "Sat":
+//                             console.log("Saturday");
+//                             break;
+//                             case "Sun":
+//                                 console.log("Sunday");
+//                                 break;
+//                                 default:
+//                                     console.log("Invalid day");
+//                                     break;
+// }
+
+
+Problem 01
+
+Write the code using if..else which would correspond to the following switch:
+
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+-----------------------------------------------------------
+Problem 02
+
+Problem
+Rewrite the code below using a single switch statement:
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+
+----------------------------------------------------------
+Problem 03
+
+Write a js program to find maximum between three numbers.
