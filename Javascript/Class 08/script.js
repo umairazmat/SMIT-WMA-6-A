ddc29db9b176e85483b029a54e09c85beb27766d
Umair Azmat <umairazmat8260@gmail.com>
2023-03-06 23:19:51 +0500

    Js class 08

---
+++ Javascript/Class 08/script.js
@@ -0,0 +1,93 @@
+// Loops
+
+// console.log("Loops");
+// console.log("Hamza");
+// console.log("Hassan");
+// console.log("Waqas");
+
+// for (let i = 0; i <= 1000 ; i++) {
+//    console.log("Hello World");
+//    console.log(i);
+// }
+
+//  let i = 11
+
+// for (let i = 0; i <= 10 ; i++) {
+//    console.log("Hello World");
+//    console.log(i);
+// }
+
+
+// While Loop
+
+// let i = 0;
+// while (i < 20) {
+//   // console.log("Hello World");
+//   // i--;
+//   // i++;
+// }
+
+// let i = 19;
+// while (i > 0) {
+//   console.log("Hello World");
+//   console.log(i);
+//   i--;
+// }
+
+
+
+// Do While Loop
+
+// let i = 0;
+// do {
+//   console.log("Hello World");
+//   i++; 
+// } while (i < 10);
+
+// Object
+// let User = {
+//   name:"Hassan",
+//   age: 20,
+//   class: "BSCS"
+// }
+// console.log(User);
+// console.log(User.name);
+// console.log(User.age);
+
+
+// For In Loop
+// for (let key in User){
+//   console.log(key);
+// }
+
+// Array
+// const Student = ["Hasan" , "Hammad", "Huzaifa"];
+// console.log(Student);
+
+// Fro Of Loop
+// for (let student of Student){
+//   console.log(student);
+// }
+
+
+// let a =6;
+// let b= 10;
+// let sum = a + b;
+// console.log(sum);
+
+// let c =60;
+// let d= 100;
+// let sum2 = c + d;
+
+
+// function sumOfTwo() {
+//       console.log(a+b);
+// }
+
+// function printHello() {
+//   console.log("Hello World");
+// }
+
+// printHello();
+// printHello();
+// printHello();
