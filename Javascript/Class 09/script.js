8d5b44db8166b7344764c333c75dfbc5935dc5d8
Umair Azmat <umairazmat8260@gmail.com>
2023-03-08 00:59:51 +0500

    js class 09

---
+++ Javascript/Class 09/script.js
@@ -0,0 +1,109 @@
+// For In Loop
+
+// let myStudents  = {
+//     student : "John",
+//     age : 25,
+//     city : "New York"
+// }
+// console.log(myStudetns);
+// console.log(myStudents.city);
+// console.log(myStudents.student);
+// console.log(myStudents["age"]);
+
+// for (let key in myStudents){
+//     console.log(key ,myStudents[key]);
+// }
+
+// function sumTwoNum() {
+
+// }
+
+// Array
+
+// const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "black", "white", "brown"];
+// console.log(colors);
+
+
+// for (let index in colors){
+//     console.log(index);
+// }
+
+// for of loop
+
+// for (let color of colors) {
+//     console.log(color);
+// }
+
+// Break Keyword
+
+// for (let i = 0; i < 20 ; i++) {
+//     if (i == 10) break;
+//     if (i %2 == 0) {
+//         console.log(i + " is even");
+//     }
+//     else {
+//         console.log(i + " is odd");
+//     }
+// }
+
+// Continue Keyword
+
+// for (let i = 0; i < 20 ; i++) {
+//     if (i %2 != 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// Data Types
+
+// let num = 10;
+// console.log(typeof num);
+
+
+
+// let num = 65.45345;
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(1));
+
+// function sumTwoNum(a, b) {
+//     return a + b;
+// }
+
+// sumTwoNum(10, 20);
+
+// let oneMillion = 1000000;
+// console.log(oneMillion);
+// console.log(typeof oneMillion);
+// // console.log(oneMillion);
+
+// "  double quotation"
+// '  single quotation'
+// `  backtick `
+
+let message = "Hello ";
+let greeting = "Good Evening";
+let name1 = "John";
+
+// Template Strings
+
+let printMesssage = `${message}  ${name1}  
+ ${greeting} 
+to SMIT Lab  ${2+2}`;
+
+console.log(printMesssage);
+
+// let str = "Hello World";
+// console.log(str.length);
+// console.log(str.trim());
+
+
+
+
+// alert  ( Math.max(10,20,60))
+// alert  ( Math.min(10,20,60))
+// console.log( Math.pow(2,2))
+//  console.log( Math.sqrt(4))
+// console.log( Math.round(4.3))
+// console.log( Math.ceil(4.5))
+// console.log( Math.floor(4.5))
