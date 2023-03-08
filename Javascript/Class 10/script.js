0046e2580e85b45a0de47764010a1a8cf7e6f246
Umair Azmat <umairazmat8260@gmail.com>
2023-03-08 23:57:53 +0500

    class 10 js

---
+++ Javascript/Class 10/script.js
@@ -0,0 +1,120 @@
+// let num = 10
+// // num = num.toUppercase();
+// console.log();
+
+// number
+// Number
+
+// const Number = {
+
+// }
+
+// let myNumber = 50;
+// let myString = "50";
+// let myFloat = 10.5;
+
+// console.log(Number.isInteger(myString))
+// console.log(Number.isInteger(myNumber))
+// console.log(Number.isInteger(myNumber) ===  myString)
+// console.log(Number.isInteger(myNumber) ==  myString)
+
+
+// let oneMillion = 1_000_000;
+// let oneMillion2 = 1000000;
+// let oneMillion = 1e6;
+// let oneMillion = 1-e6;
+// console.log(oneMillion);
+
+// let pI = 3.1415926535897932384626433832795;
+// console.log(pI.toFixed(2));
+
+
+// Math Object & Math Methods
+
+// console.log(Math);
+// console.log(Math.pow(2,3))
+// console.log(Math.floor(Math.random()*10))
+// console.log(Math.floor(Math.random()*100))
+// console.log(Math.max(1,2,3,4,5,6,7,8,9,10))
+// console.log(Math.min(1,2,3,4,5,6,7))
+// console.log(Math.sqrt(25));
+// console.log(Math.round(50.6));
+// console.log(Math.round(50.4));
+// console.log(Math.round(50.5));
+// console.log(Math.ceil(50.4));
+// console.log(Math.floor(50.4));
+
+// function addTwoNumbers(num1, num2) {
+//     return num1 + num2;
+// }
+
+// addTwoNumbers(10, 20);
+
+// Strings
+
+// let myString = "String Using Double Quotes";
+// let myString2 = 'String Using Single Quotes';
+// let myString3 = `String Using ${myString} Backticks`;
+
+// + " " +
+
+// \n
+// \t
+// \\
+
+// let question = "Teacher Asked to "Student": What is your name?"; //error
+// let question = 'Teacher did't Asked to "Student": What is your name?';
+// let question = "Teacher did't Asked to \"Student \": \n What is your \t name?";
+// console.log(question);
+
+
+// let comment = "Welcome TO JS class of SMIT Lab";
+// console.log(comment);
+// console.log(comment.length);
+// console.log(comment.toUpperCase());
+// console.log(comment.toLowerCase());
+
+// for ( let index in comment) {
+//     console.log(index,comment[index]);
+// }
+
+// console.log(comment.indexOf("a"));
+
+// console.log(comment.indexOf("a"));
+// console.log(comment.lastIndexOf("a"));
+// console.log(comment.includes("class"));
+// console.log(comment.includes("Class"));
+// console.log(comment.startsWith("Welcome")); 
+// console.log(comment.endsWith("Lab"));
+// console.log(comment.charAt(16));
+// console.log(comment.slice(0,7));
+// console.log(comment.slice(14,20));
+
+// Date Object Methods
+
+// console.log(Date());
+
+// let currentDate = new Date();
+// console.log(currentDate);
+// console.log(currentDate.getDate());
+// console.log(currentDate.getDay());
+// console.log(currentDate.getFullYear());
+// console.log(currentDate.getHours());
+// console.log(currentDate.getMinutes());
+// console.log(currentDate.getSeconds());
+// console.log(currentDate.getMilliseconds());
+// console.log(currentDate.getTime());
+// console.log(currentDate.getTimezoneOffset());
+// console.log(currentDate.getUTCDate());
+// console.log(currentDate.getUTCDay());
+// console.log(currentDate.getUTCFullYear());
+// console.log(currentDate.getUTCHours());
+// console.log(currentDate.getUTCMinutes());
+// console.log(currentDate.getUTCSeconds());
+// console.log(currentDate.getUTCMilliseconds());
+
+// const date = new Date("2020-05-12T23:50:21.817Z");
+// console.log(date);
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
